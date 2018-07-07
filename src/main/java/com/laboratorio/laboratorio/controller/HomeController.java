b99f328fba63a04cd43b90ff92e679562f1fb28d
danielrpg <daniel.fernandezgp@gmail.com>
2018-07-06 21:11:57 -0400

    Primer Cambio

---
+++ src/main/java/com/laboratorio/laboratorio/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.laboratorio.laboratorio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String getHomePage() {
+        return "Hola";
+    }
+}
